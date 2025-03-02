66731cd72415c7b179bc68c3045211c8718523ae
Parves24-dev <parveshossainemon18@gmail.com>
2025-03-03 05:46:16 +0600

    problem fix

--- index.js
+++ index.js
@@ -15,41 +15,6 @@ document.getElementById('color').addEventListener('click', function themes() {
 
 
 
-
-
-
-
-
-
-
-
-// Get all the "Completed" buttons inside the cards
-var completeButtons = document.querySelectorAll('.btn.bg-sky-500');
-
-// Get the Clear History section to add the comment
-var clearHistorySection = document.querySelector('.col-span-1.bg-white');
-
-// Loop through each "Completed" button
-for (var i = 0; i < completeButtons.length; i++) {
-    completeButtons[i].addEventListener('click', function() {
-        
-        // Get the card title for the clicked button
-        var cardTitle = this.closest('.bg-sky-50').querySelector('h1').textContent;
-        
-        // Create a new comment element for the Clear History section
-        var comment = document.createElement('div');
-        comment.classList.add('comment', 'p-3', 'border-b', 'border-dashed', 'border-gray-300');
-        
-        // Add some content to the comment
-        comment.innerHTML = '<strong>Completed Task:</strong> ' + cardTitle + ' <br> <span>Comment: Task completed successfully!</span>';
-        
-        // Append the comment to the Clear History section
-        clearHistorySection.appendChild(comment);
-    });
-}
-
-
-
     //   clear button
         document.getElementById('clear-btn').addEventListener('click', function () {
         document.getElementById('comment-box').innerText = "";
